3dc5a199cdae3eefb5f511b2937760510f7fc518
NitzanSmulevici <nitzanye@gmail.com>
2023-03-27 15:38:01 +0300

    refactor images array

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -5,32 +5,19 @@ interface ImageType {
 }
 
 const ImageContainer = ({ images }: ImageType) => {
+  const firstTwoImages = images.slice(0, 2); 
+
   return (
     <div className="image-container">
-    {images.length <= 1 && (
-      <div className='solo-wrapper'>   
-      <img
-        src={images[0]}
-        alt="Item"
-        className="full-image"
-      />
-      </div>
-    )}
-    {images.length > 1 && (
-      <div className='image-wrapper'>     
-      <img
-        src={images[0]}
-        alt="First Item"
-        className="image-item"
-      />
-      <img
-        src={images[1]}
-        alt="Second Item"
-        className="image-item"
-      />
-      </div>
-    )}
-  </div>
+      {firstTwoImages.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Item ${index}`}
+          className={`image-item ${firstTwoImages.length > 1 ? 'multiple-images' : ''}`}
+        />
+      ))}
+    </div>
   );
 }
 
